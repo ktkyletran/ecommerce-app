1f3564ca3674b06a680505c25507fb2d7fc47b90
Vanity <ktkyletran@gmail.com>
2021-08-22 15:32:06 -0500

    fetches cart and added routers/links

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react'
-import { Products, Navbar } from './components';
+import { Products, Navbar, Cart } from './components';
 import { commerce } from './lib/commerce';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 const App = () => {
   const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState({});
 
   const fetchProducts = async () => {
     const { data } = await commerce.products.list();
@@ -11,17 +13,37 @@ const App = () => {
     setProducts(data);
   };
 
+  const fetchCart = async () => {
+    setCart(await commerce.cart.retrieve());
+  };
+
+  const addItem = async (productId, quantity) => {
+    const item = await commerce.cart.add(productId, quantity);
+
+    setCart(item.cart)
+  }
+
   useEffect(() => {
     fetchProducts();
+    fetchCart();
   }, []);
 
-  console.log(products)
+  console.log(cart)
 
   return (
-    <div>
-      <Navbar />
-      <Products products={products}/>
-    </div>
+    <Router>
+      <div>
+        <Navbar totalItems={cart.total_items}/>
+        <Switch>
+          <Route exact path="/">
+            <Products products={products} onAddToCart={addItem} />
+          </Route>
+          <Route exact path="/cart">
+            <Cart cart={cart}/>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   )
 }
 
